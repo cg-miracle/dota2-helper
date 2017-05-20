8a193f0b78be097fbe643317b879b9f6a046656b
Miracle <337544754@qq.com>
2017-05-20 17:56:05 +0800

    重写比赛表格

--- src/page/userDetail.vue
+++ src/page/userDetail.vue
@@ -19,7 +19,22 @@
       <section class='recent-match'>
         <h2>最近比赛</h2>
         <section class='match-table'>
-          <div class='table-header'></div>
+          <div class='table-header'>
+              <span>英雄</span>
+              <span>结果</span>
+              <span>level</span>
+              <span>结束时间</span>
+              <span>KDA</span>
+          </div>
+          <div class='table-body'>
+            <div class='cell' v-for='match in tableData'>
+              <span class="item">{{match.hero}}</span>
+              <span class="item"><span class="resultTag" :class="match.result === '胜'?'winColor':'failColor'">{{match.result}}</span></span>
+              <span class="item col-C7CBCF">{{match.level}}</span>
+              <span class="item">{{match.end_at}}</span>
+              <span class="item">{{match.kda}}</span>
+            </div>
+          </div>
         </section>
       </section>
     </section>
@@ -28,156 +43,162 @@
 </template>
 
 <script>
-import hd from '../components/header.vue'
-import tb from '../components/toolbar.vue'
-import axios from 'axios'
-import util from '../lib/utils'
+  import hd from '../components/header.vue'
+  import tb from '../components/toolbar.vue'
+  import axios from 'axios'
+  import util from '../lib/utils'
 
-export default {
-  data () {
-    return {
-      userInfo: {// 用户信息
-        steamid: '1112'
-      },
-      sid: '', // stemid
-      did: '', // dota2 数字id
-      imgurl: '',
-      dotaid: '',
-      tips: '请输入你要查找的玩家',
-      tableData: [{
-        end_at: '19小时前',
-        result: '胜',
-        hero: '斧王',
-        level: 'High',
-        kda: '40'
-      }, {
-        end_at: '19小时前',
-        result: '胜',
-        hero: '斧王',
-        level: 'High',
-        kda: '40'
-      }],
-      matchIds: [],
-      matchDetils: []
-    }
-  },
-  mounted () {
-    this.sid = this.$route.params.sid
-    this.did = this.getDotaid(this.sid)
-    this.getUsers()
-    this.getmatchIds()
-  },
-  methods: {
-    // 获取用户信息 头像，数字ID等
-    getUsers () { // 221829218
-      let steamids = [this.sid]
-      axios({
-        method: 'get',
-        url: '/api/ISteamUser/GetPlayerSummaries/v0002/?key=' + util.config.dota2_token + '&steamids=' + steamids
-      }).then((res) => {
-        this.userInfo = res.data.response.players[0]
-      }).catch((err) => {
-        console.log(err)
-      })
+  export default {
+    data () {
+      return {
+        userInfo: {// 用户信息
+          steamid: '1112'
+        },
+        sid: '', // stemid
+        did: '', // dota2 数字id
+        imgurl: '',
+        dotaid: '',
+        tips: '请输入你要查找的玩家',
+        tableData: [{
+          end_at: '19小时前',
+          result: '胜',
+          hero: '斧王',
+          level: 'High',
+          kda: '40'
+        }],
+        matchIds: [],
+        matchDetils: []
+      }
     },
-    // 获取最近5场比赛的 比赛id
-    getmatchIds () {
-      let accountid = this.getDotaid(this.sid)
-      axios({
-        method: 'get',
-        url: '/api/IDOTA2Match_570/GetMatchHistory/v1?key=' + util.config.dota2_token + '&matches_requested=5&account_id=' + accountid
-      }).then((res) => {
-        let matches = res.data.result.matches
-        this.matchIds = matches.map(function (value) {
-          return value.match_id
-        })
-        this.getmatchDetail()
-      }).catch((err) => {
-        console.error(err)
-      })
+    mounted () {
+      this.sid = this.$route.params.sid
+      this.did = this.getDotaid(this.sid)
+      this.getUsers()
+      this.getmatchIds()
     },
-    getmatchDetail () {
-      // 创建返回值为promise对象的函数数组
-      let promisify = this.matchIds.map(value => {
-        return () => {
-          return this.getDetailAjax(value)
-        }
-      })
-      // 利用reduce 进行循环promise操作 设置初始值initialValue 为Promise.resolve()
-      promisify.reduce((pre, next) => {
-        return pre.then(next)
-      }, Promise.resolve()).then((result) => {
-        this.getShowData()
-      }).catch(err => {
-        console.error(err)
-      })
-    },
-    // 获取最近5场比赛的详情
-    getDetailAjax (id) {
-      return new Promise((resolve, reject) => {
+    methods: {
+      // 获取用户信息 头像，数字ID等
+      getUsers () { // 221829218
+        let steamids = [this.sid]
         axios({
           method: 'get',
-          url: '/api/IDOTA2Match_570/GetMatchDetails/v1?key=' + util.config.dota2_token + '&match_id=' + id
+          url: '/api/ISteamUser/GetPlayerSummaries/v0002/?key=' + util.config.dota2_token + '&steamids=' + steamids
         }).then((res) => {
-          let details = res.data.result
-          this.matchDetils.push(details)
-          resolve('ok')
+          this.userInfo = res.data.response.players[0]
         }).catch((err) => {
           console.log(err)
-          reject(err)
         })
-      })
-    },
-    // 得到列表数据
-    getShowData () {
-      // radiant_win true Or false
-      this.tableData = this.matchDetils.map(match => {
-        let players = match.players
-        let player = players.filter((value) => {
-          if (value.account_id === this.did) {
-            return value
+      },
+      // 获取最近5场比赛的 比赛id
+      getmatchIds () {
+        let accountid = this.getDotaid(this.sid)
+        axios({
+          method: 'get',
+          url: '/api/IDOTA2Match_570/GetMatchHistory/v1?key=' + util.config.dota2_token + '&matches_requested=5&account_id=' + accountid
+        }).then((res) => {
+          let matches = res.data.result.matches
+          this.matchIds = matches.map(function (value) {
+            return value.match_id
+          })
+          this.getmatchDetail()
+        }).catch((err) => {
+          console.error(err)
+        })
+      },
+      getmatchDetail () {
+        // 创建返回值为promise对象的函数数组
+        let promisify = this.matchIds.map(value => {
+          return () => {
+            return this.getDetailAjax(value)
           }
-        })[0]
-        let rtn = {}
-        let winStatus = ''
-        let engine = util.get8bitNumber(player.player_slot).substr(0, 1) // 阵营
-        if (!parseInt(engine)) { // 天辉
-          winStatus = match.radiant_win ? '胜' : '负'
-        } else { // 夜宴
-          winStatus = match.radiant_win ? '负' : '胜'
-        }
-        rtn.result = winStatus
-        util.getHeroNameFromId(player.hero_id, (name) => {
-          rtn.hero = name
         })
-        var diff = Date.now() - match.start_time * 1000
-        rtn.end_at = util.getLastTimeStr(diff)
-        rtn.level = 'Normal' // level 不知道怎么算 ＝ ＝！
-        rtn.kda = util.getKDA(player.kills, player.deaths, player.assists)
-        console.log(util.getKDA(player.kills, player.deaths, player.assists))
-        return rtn
-      })
-      console.log(this.tableData)
+        // 利用reduce 进行循环promise操作 设置初始值initialValue 为Promise.resolve()
+        promisify.reduce((pre, next) => {
+          return pre.then(next)
+        }, Promise.resolve()).then((result) => {
+          this.getShowData()
+        }).catch(err => {
+          console.error(err)
+        })
+      },
+      // 获取最近5场比赛的详情
+      getDetailAjax (id) {
+        return new Promise((resolve, reject) => {
+          axios({
+            method: 'get',
+            url: '/api/IDOTA2Match_570/GetMatchDetails/v1?key=' + util.config.dota2_token + '&match_id=' + id
+          }).then((res) => {
+            let details = res.data.result
+            this.matchDetils.push(details)
+            resolve('ok')
+          }).catch((err) => {
+            console.log(err)
+            reject(err)
+          })
+        })
+      },
+      // 得到列表数据
+      getShowData () {
+        // radiant_win true Or false
+        this.tableData = this.matchDetils.map(match => {
+          let players = match.players
+          let player = players.filter((value) => {
+            if (value.account_id === this.did) {
+              return value
+            }
+          })[0]
+          let rtn = {}
+          let winStatus = ''
+          let engine = util.get8bitNumber(player.player_slot).substr(0, 1) // 阵营
+          if (!parseInt(engine)) { // 天辉
+            winStatus = match.radiant_win ? '胜' : '负'
+          } else { // 夜宴
+            winStatus = match.radiant_win ? '负' : '胜'
+          }
+          console.log(this.isWin)
+          rtn.result = winStatus
+          util.getHeroNameFromId(player.hero_id, (name) => {
+            rtn.hero = name
+          })
+          var diff = Date.now() - match.start_time * 1000
+          rtn.end_at = util.getLastTimeStr(diff)
+          rtn.level = 'Normal' // level 不知道怎么算 ＝ ＝！ Normal,High,Very High
+          rtn.kda = util.getKDA(player.kills, player.deaths, player.assists)
+          return rtn
+        })
+        console.log(this.tableData)
+      },
+      getSteamid (dotaid) {
+        return util.dotaidToSteamid(dotaid)
+      },
+      getDotaid (steamid) {
+        return util.steamidToDotaid(steamid)
+      },
+      backfunc () {
+        util.backTo()
+      }
     },
-    getSteamid (dotaid) {
-      return util.dotaidToSteamid(dotaid)
-    },
-    getDotaid (steamid) {
-      return util.steamidToDotaid(steamid)
-    },
-    backfunc () {
-      util.backTo()
+    components: {
+      hd,
+      tb
     }
-  },
-  components: {
-    hd,
-    tb
   }
-}
 </script>
 
 <style lang='scss' style="stylesheet/scss">
 @import '../assets/scss/common.scss';
+$cellBorderColor: #F1F2F2;
+$failColor: #BBBDCF;
+$winColor: #31A836;
+.col-C7CBCF{
+  color: #C7CBCF;
+}
+.failColor{
+  background-color: $failColor;
+}
+.winColor{
+  background-color: $winColor;
+}
 #user-detail{
   //用户信息
   .user-info{
@@ -224,8 +245,41 @@ export default {
     }
   }
   //最近比赛
-  .el-table-head{
-    color: blue;
+  .match-table{
+    .table-header{
+      display: flex;
+      font-size: 15px;
+      background-color: rgb(254,255,255);
+      color: #C7CBCF;
+      justify-content: space-around;
+      border-bottom: 1px solid $cellBorderColor;
+      padding: 5px 10px;
+      span{
+         flex: 0 1 20%;
+      }
+    }
+    .table-body{
+      background-color: #fff;
+      font-size: 14px;
+      .cell{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        border-bottom: 1px solid $cellBorderColor;
+        padding: 10px 10px;
+        .item{
+          flex: 0 1 20%;
+          .resultTag{
+            display:inline-block;
+            width:34px;
+            height:34px;
+            line-height: 34px;
+            text-align:center;
+            color: white;
+          }
+        }
+      }
+    }
   }
 }
 </style>
